4deff6f380192c50c3b973fc2083713c7f74d7aa
Eman Nollase <eman.nollase@gamificationnation.com>
2022-06-18 00:21:14 +0800

    get the list of weather forecast from openweather api

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
 import json
-import urllib.request
+from datetime import datetime
+from urllib import request as http_client
 from flask import Flask, render_template, abort, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def weather():  # put application's code here
-    results = urllib.request.urlopen('https://raw.githubusercontent.com/eesur/country-codes-lat-long/master/country-codes-lat-long-alpha3.json').read()
+    results = http_client.urlopen('https://raw.githubusercontent.com/eesur/country-codes-lat-long/master/country-codes-lat-long-alpha3.json').read()
     _results = json.loads(results)
     countries = _results["ref_country_codes"]
     country_list = list()
@@ -20,17 +21,36 @@ def weather():  # put application's code here
 
 @app.route("/forecast", methods=['POST'])
 def forecast():
-    api_key = '1f88cf5ad23f2d0ec9f6613c4508ef8c'
-    params = request.json()
-    print(params)
-
+    params = request.get_json()
+    current_weather_url = f"http://api.openweathermap.org/data/2.5/weather?{params['country']}&units=metric&appid=1f88cf5ad23f2d0ec9f6613c4508ef8c"
+    forecast_weather_url = f"http://api.openweathermap.org/data/2.5/forecast?{params['country']}&units=metric&appid=1f88cf5ad23f2d0ec9f6613c4508ef8c"
     try:
-        # source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid='+api_key).read()
-        pass
+        data = dict()
+        current_source = http_client.urlopen(current_weather_url).read()
+        _curr_data = json.loads(current_source)
+        _current = dict()
+        _current["description"] = _curr_data['weather'][0]['description']
+        _current["icon"] = "https://www.weatherbit.io/static/img/icons/t"+_curr_data['weather'][0]['icon']+".png"
+        _current["temp"] = _curr_data['main']['temp']
+        data['current_weather'] = _current
+
+        forecast_source = http_client.urlopen(forecast_weather_url).read()
+        _forecast_data = json.loads(forecast_source)
+        forecast_list = _forecast_data['list'] if _forecast_data['cnt'] < 24 else _forecast_data['list'][:24]
+        forecast_weather = list()
+        for weather in forecast_list:
+            _tmp = dict()
+            _tmp["description"] = weather['weather'][0]['description']
+            _tmp["icon"] = "https://www.weatherbit.io/static/img/icons/t"+weather['weather'][0]['icon']+".png"
+            _tmp["temp"] = weather['main']['temp']
+            _tmp["date_time"] = datetime.fromtimestamp(weather['dt'])
+            forecast_weather.append(_tmp)
+
+        data["forecast_weather"] = forecast_weather
+        return jsonify(data)
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
 
-    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
