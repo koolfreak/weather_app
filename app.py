2818cd813fbdea94c0fdb90dcd23ddba178f0d62
Eman Nollase <eman.nollase@gamificationnation.com>
2022-06-17 23:15:52 +0800

    display the list of countries

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 
 import json
 import urllib.request
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def weather():  # put application's code here
+    results = urllib.request.urlopen('https://raw.githubusercontent.com/eesur/country-codes-lat-long/master/country-codes-lat-long-alpha3.json').read()
+    _results = json.loads(results)
+    countries = _results["ref_country_codes"]
+    country_list = list()
+    for country in countries:
+        _tmp = dict()
+        _tmp[country['country']] = "lat="+str(country["latitude"])+"&lon="+str(country["longitude"])
+        country_list.append(_tmp)
 
-    return render_template("index.html", data={})
+    return render_template("index.html", countries=country_list)
 
-@app.route("/forecast/current", methods=['POST'])
+@app.route("/forecast", methods=['POST'])
 def forecast():
+    api_key = '1f88cf5ad23f2d0ec9f6613c4508ef8c'
+    params = request.json()
+    print(params)
 
-    pass
+    try:
+        # source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid='+api_key).read()
+        pass
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
